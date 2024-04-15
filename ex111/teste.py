414173353b5c4a00096d198649e4df500f5c4d55
Lucas Renan <lucas.lcs10@live.com>
2024-04-15 10:28:28 -0300

    ex112

---
+++ ex111/teste.py
@@ -0,0 +1,4 @@
+from ex112.utilidadescev import moeda
+
+num = float(input('Digite o preço: R$'))
+moeda.resumo(num, 10, 5)
---
+++ ex112/teste.py
@@ -0,0 +1,4 @@
+from ex112.utilidadescev import moeda
+from ex112.utilidadescev import dado
+num = dado.leiaDinheiro('Digite um preço: R$')
+moeda.resumo(num, 10, 5)
