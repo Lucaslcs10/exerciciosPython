600dc6aca51a58993a5d46c89164187f2060c366
Lucas Renan <lucas.lcs10@live.com>
2024-06-07 20:13:16 -0300

    ex113

--- ex113.py
+++ ex113.py
@@ -1,7 +1,4 @@
 from utilidades import leia
-vermelho = '\033[31m'
-normal = '\033[m'
-amarelo = '\033[33m'
 
 ni = leia.Int('Digite um número INTEIRO: ')
 nr = leia.Float('Digite um número REAL: ')
