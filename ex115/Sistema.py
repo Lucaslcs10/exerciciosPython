209c7712160c7c39d9f04807de45ca186a5c5c71
Lucas Renan <lucas.lcs10@live.com>
2024-06-14 12:52:12 -0300

    ex115c

--- ex115/Sistema.py
+++ ex115/Sistema.py
@@ -7,10 +7,13 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['VER CADASTROS', 'NOVO CADASTRO', 'SAIR DO SISTEMA'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        cabeçalho('LISTA DE PESSOAS CADASTRADAS')
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = Int('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema...')
         break
--- ex115/lib/arquivo/__init__.py
+++ ex115/lib/arquivo/__init__.py
@@ -1,6 +1,7 @@
 import cores
 from ex115.lib.interface import *
 
+
 def arquivoExiste(nome):
     try:
         a = open(nome, 'rt')
@@ -28,5 +29,24 @@ def lerArquivo(nome):
         print(f'Erro ao inicializar o arquivo {cores.vermelho}{nome}{cores.normal}')
     else:
         cabeçalho('PESSOAS CADASTRADAS')
-        print(a.read())
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<32} {dado[1]:3>} anos')
+    finally:
+        a.close()
 
+
+def cadastrar(arq, nome='Desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print(f'Erro ao inicializar o arquivo {cores.vermelho}{arq}{cores.normal}')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Erro ao escrever dados')
+        else:
+            print(f'Novo registro de {verde}{nome}{normal} adicionado com sucesso!')
+            a.close()
